473c4f20591a144da3c70075c63fa69c38a0f639
John Berninger <john.berninger@gmail.com>
2018-05-03 14:02:23 +0000

    add argument parsing, XLSX output format to host collection report

--- errataByHostCollection.py
+++ errataByHostCollection.py
@@ -5,27 +5,42 @@ import pprint
 import json
 import collections
 from pyexcel_ods import save_data
-
+import argparse
+import xlsxwriter
 from satellite import Satellite
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--debug", help="Print debugging messages", action="count")
+parser.add_argument("-o", "--outfmt", help="Output format", choices=['xlsx', 'ods'], default='ods')
+
+args = parser.parse_args()
 
 errataWorkbook = collections.OrderedDict()
 listOfHostCollections = {}
 listOfErrata = {}
 
+def printDBG(level, message):
+  if level>args.debug:
+    return
+  print(message)
+              
 with open('credentials.json') as credentialfile:
   credentials = json.load(credentialfile)
-
+printDBG(3, "Satellite credentials loaded")
 
 def collectData():
+  printDBG(1, "Collecting data from Satellite")
   s = Satellite(credentials['hostname'])
   s.setUsername(credentials['username'])
   s.setPassword(credentials['password'])
   hcs=s.listHostCollections()
   for hc in hcs:
+    printDBG(2, "Examining host collection "+hc['name'])
     hcInfo = s.getHostCollection(hc['id'])
     hcInfo['errata'] = {}
     hcInfo['errataRebootSuggested'] = False
     for id in hcInfo['host_ids']:
+      printDBG(3, "Examining collection member host")
       errata = s.getHostErrata(str(id))
       for erratum in errata:
         hcInfo['errataRebootSuggested'] = hcInfo['errataRebootSuggested'] or erratum['reboot_suggested']
@@ -65,8 +80,60 @@ def generateODS():
     errataWorkbook.update({errataID:errataSheet})
               
   save_data('erratacollectioninfo.ods', errataWorkbook)
+
+def generateXLSX():
+  printDBG(1, "Generating report in XLSX format")
+  workbook = xlsxwriter.Workbook('errataByHostCollection.xlsx')
+  topSheet = workbook.add_worksheet('Host Collections Report')
+  topSheetRow = 0
+  
+  printDBG(2, "Generating host collection sheets")
+  for hcName in sorted(listOfHostCollections):
+    hcErrata = listOfHostCollections[hcName]['errata']
+    thisSheet = workbook.add_worksheet(hcName)
+    thisSheet.write(0, 0, "Errata Name")
+    thisSheet.write(0, 1, "Errata Type")
+    thisSheet.write(0, 2, "Number of packages")
+    thisSheet.write(0, 3, "Reboot Suggested?")
+    topSheet.write(topSheetRow, 0, hcName)
+    topSheet.write(topSheetRow, 1, len(hcErrata))
+    topSheet.write(topSheetRow, 2, 'REBOOT' if listOfHostCollections[hcName]['errataRebootSuggested'] else '')
+    topSheetRow+=1
+    hostSheetRow = 1
+    for errataName in sorted(hcErrata):
+      errataInfo = hcErrata[errataName]
+      thisSheet.write(hostSheetRow, 0, errataName)
+      thisSheet.write(hostSheetRow, 1, errataInfo['type'])
+      thisSheet.write(hostSheetRow, 2, len(errataInfo['packages']))
+      thisSheet.write(hostSheetRow, 3, 'REBOOT' if errataInfo['reboot_suggested'] else '')
+      hostSheetRow+=1
+        
+  printDBG(2, "Generating errata sheets")
+  for errataName in sorted(listOfErrata):
+    errata = listOfErrata[errataName]
+    errataID = errata['errata_id'].replace(":", "-")
+    errataSheet = workbook.add_worksheet(errataID)
+    errataSheet.write(0, 0, "ID:")
+    errataSheet.write(0, 1, errata['errata_id'])
+    errataSheet.write(1, 0, "Name:")
+    errataSheet.write(1, 1, errata['name'])
+    errataSheet.write(2, 0, "Type:")
+    errataSheet.write(2, 1, errata['type'])
+    errataSheet.write(3, 0, "Reboot suggested:")
+    errataSheet.write(3, 1, 'YES' if errata['reboot_suggested'] else 'NO')
+    errataSheet.write(4, 0, "Affected packages:")
+    errataSheetRow = 5
+    for pkg in errata['packages']:
+      errataSheet.write(errataSheetRow, 1, pkg)
+      errataSheetRow+=1
+      
+  printDBG(2, "Saving workbook")
+  workbook.close()
+
               
 if __name__ == '__main__':
   collectData()
-  generateODS()
-
+  if args.outfmt == 'ods':
+    generateODS()
+  elif args.outfmt == 'xlsx':
+    generateXLSX()
